4c8380370d76a51ace979a739cdcdea15cbd3eac
kohlbauers190178 <kohlbauers190178@sus.htl-grieskirchen.at>
2022-09-26 10:08:12 +0200

    implemented logic for NumberTests

--- kohlbauersimon/src/Main.java
+++ kohlbauersimon/src/Main.java
@@ -8,10 +8,30 @@ public class Main {
         NumberTester numberTester = new NumberTester(FILEPATH);
 
         NumberTest isEven = (p) -> p%2==0;
+        NumberTest isPrime = (p) -> {
+            for (int i = 2; i <= p / 2; ++i) {
+                if (p % i == 0) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        NumberTest isPalindrome = (p) -> {
 
-        //TODO: ausprogrammian
-        NumberTest isPrime = (p) -> true;
-        NumberTest isPalindrome = (p) -> true;
+            int number =p;
+            int reverse =0;
+
+            while(number!=0){
+                int remaining = number%10;
+                reverse = reverse*10+remaining;
+                number = number/10;
+            }
+
+            if(reverse == p){
+                return true;
+            }
+            return false;
+        };
 
         numberTester.setOddEvenTester(isEven);
         numberTester.setPrimeTester(isPrime);
